74badc40fb170a4b662f0eb247dc225118e5a6aa
kurt t stam <kstam@redhat.com>
2017-10-02 08:17:20 -0400

    hello

---
+++ src/main/java/org/r41n/Hello.java
@@ -0,0 +1,15 @@
+
+package org.r41n;
+
+public class Hello {
+
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+	
+	public String print() {
+		return "Hey";
+	}
+	
+}
